d060e9ac52d7dc171ffc9e2d600f4bfe47c26635
Bob-Maryville <rbatzer1@live.maryville.edu>
2023-12-16 10:28:26 -0600

    Changing code to python

---
+++ SWDV460_code.py
@@ -0,0 +1,8 @@
+import xml.etree.ElementTree as ET
+
+tree = ET.parse("C:/Users/bobba/OneDrive/Desktop/School/SWDV_460/catalog.xml")
+root = tree.getroot()
+
+for book in root.findall('book'):
+    title = book.find('title').text
+    print(title)
